ff5becfd311c9553ef058bb45e6cf44a7feb277b
Jeff Hensley <jeffrey_hensley@hotmail.com>
2017-03-05 01:01:56 -0500

    Admin tab for status page
    does not submit changes yet
    
    only populating for router mode, need to add mesh mode

--- chip/var/www/html/status.php
+++ chip/var/www/html/status.php
@@ -21,29 +21,28 @@
 
 <div data-role="page" id="page_status" data-theme="<?php echo $theme; ?>">
   <div data-role="header">
-    <a href="#page_status" class="ui-btn ui-corner-all ui-shadow ui-icon-home ui-btn-icon-left ui-btn-active">Home</a>
-	<!--<a href="#page_home_info" class="ui-disabled ui-btn ui-corner-all ui-shadow ui-icon-info ui-btn-icon-left">Help</a>-->
-	<a href="#page_home_info" class="ui-btn ui-corner-all ui-shadow ui-icon-info ui-btn-icon-left">Help</a>
+    <a href="#page_status" class="ui-btn ui-corner-all ui-shadow ui-icon-home ui-btn-icon-left">Home</a>
+	<a href="#page_status_info" data-rel="popup" class="ui-btn ui-corner-all ui-shadow ui-icon-info ui-btn-icon-left">Help</a>
     <h1><?php echo shell_exec("hostname"); ?> - Micro Mesh</h1>
     <div data-role="navbar">
       <ul>
-        <li><a href="#page_status" data-transition="slide">Status</a></li>
+        <li><a href="#page_status" class="ui-btn-active">Status</a></li>
 		<?php
     		switch (1) {
         		case (file_exists('/var/www/flags/.micromesh')) :
 					?>
-			        <li><a href="#page_nodes">Nodes</a></li>
+			        <li><a href="#page_nodes" data-transition="slide">Nodes</a></li>
 			        <li><a href="#page_olsr" data-transition="slide">OLSR</a></li>
 		            <?php
 					break;
         		case (file_exists('/var/www/flags/.microrouter')) :
 					?>
-			        <li><a href="#page_clients">Clients</a></li>
+			        <li><a href="#page_clients" data-transition="slide">Clients</a></li>
 		            <?php
 					break;
 		    }   
 		?>
-        <li><a href="/setup.php" data-ajax="false" data-transition="slide">Setup</a></li>
+        <li><a href="#page_admin" data-transition="slide">Admin</a></li>
       </ul>
     </div>
   </div>
@@ -131,6 +130,13 @@
   <div data-role="footer">
     <h1>Valley Digital Network (VDN)</h1>
   </div>
+  <div data-role="popup" id="page_status_info">
+    <a href="#" data-rel="back" class="ui-btn ui-corner-all ui-shadow ui-btn ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a>
+    <br />
+    <p>
+        Status page help
+    </p>
+  </div>
 </div> 
 
 
@@ -143,22 +149,9 @@
     <div data-role="navbar">
       <ul>
         <li><a href="#page_status" data-transition="reverse">Status</a></li>
-        <?php
-            switch (1) {
-                case (file_exists('/var/www/flags/.micromesh')) :
-                    ?>  
-                    <li><a href="#page_nodes">Nodes</a></li>
-                    <li><a href="#page_olsr" data-transition="slide">OLSR</a></li>
-                    <?php
-                    break;
-                case (file_exists('/var/www/flags/.microrouter')) :
-                    ?>  
-                    <li><a href="#page_clients">Clients</a></li>
-                    <?php
-                    break;
-            }   
-        ?>  
-        <li><a href="/setup.php" data-ajax="false" data-transition="slide">Setup</a></li>
+        <li><a href="#page_nodes" class="ui-btn-active">Nodes</a></li>
+        <li><a href="#page_olsr" data-transition="slide">OLSR</a></li>
+        <li><a href="#page_admin" data-transition="slide">Admin</a></li>
       </ul>
     </div>
   </div>
@@ -175,22 +168,8 @@
   <div data-role="popup" id="page_nodes_info">
     <a href="#" data-rel="back" class="ui-btn ui-corner-all ui-shadow ui-btn ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a>
     <br />
-    <p> There are two types of installations that can be selected. </p>
     <p> 
-        The <b>Micro Mesh Node</b> installation will create a mesh node using one of the two wifi radios
-        available on the chip. This node will be able to communicate with other mesh nodes in range over
-        that wifi radio.  The remaining wifi radio on the chip will be used as an Access Point, which 
-        allows direct connection to the mesh from personal devices such as: smart phones, tablets, laptops,
-        etc.  If an ethernet connection is also available on the chip, it can be used to provide WAN or LAN 
-        access.
-    </p>
-    <p>
-        The <b>Micro Mesh Router</b> installation does not create a mesh node.  Instead, this choice will
-        only create an Access Point on the chip using one of the wifi radios.  Additionally, the chip will
-        need to have an ethernet connection to a LAN port on an existing mesh node.  All traffic from the
-        Access Point is routed to the ethernet connected mesh node.  This allows direct connection to the
-        mesh from personal devices such as: smart phones, tablets, laptops, etc. using the chip as a
-        router.
+		Nodes page help
     </p>
   </div>
 </div>
@@ -205,22 +184,9 @@
     <div data-role="navbar">
       <ul>
         <li><a href="#page_status" data-transition="reverse">Status</a></li>
-        <?php
-            switch (1) {
-                case (file_exists('/var/www/flags/.micromesh')) :
-                    ?>  
-                    <li><a href="#page_nodes">Nodes</a></li>
-                    <li><a href="#page_olsr" data-transition="slide">OLSR</a></li>
-                    <?php
-                    break;
-                case (file_exists('/var/www/flags/.microrouter')) :
-                    ?>  
-                    <li><a href="#page_clients">Clients</a></li>
-                    <?php
-                    break;
-            }   
-        ?>  
-        <li><a href="/setup.php" data-ajax="false" data-transition="slide">Setup</a></li>
+        <li><a href="#page_nodes" data-transition="reverse">Nodes</a></li>
+        <li><a href="#page_olsr" class="ui-btn-active">OLSR</a></li>
+        <li><a href="#page_admin" data-transition="slide">Admin</a></li>
       </ul>
     </div>
   </div>
@@ -237,22 +203,8 @@
   <div data-role="popup" id="page_olsr_info">
 	<a href="#" data-rel="back" class="ui-btn ui-corner-all ui-shadow ui-btn ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a>
 	<br />
-	<p> There are two types of installations that can be selected. </p>
 	<p>
-		The <b>Micro Mesh Node</b> installation will create a mesh node using one of the two wifi radios
-		available on the chip. This node will be able to communicate with other mesh nodes in range over
-		that wifi radio.  The remaining wifi radio on the chip will be used as an Access Point, which 
-		allows direct connection to the mesh from personal devices such as: smart phones, tablets, laptops,
-		etc.  If an ethernet connection is also available on the chip, it can be used to provide WAN or LAN
-		access.
-	</p>
-	<p>
-		The <b>Micro Mesh Router</b> installation does not create a mesh node.  Instead, this choice will
-		only create an Access Point on the chip using one of the wifi radios.  Additionally, the chip will
-		need to have an ethernet connection to a LAN port on an existing mesh node.  All traffic from the
-		Access Point is routed to the ethernet connected mesh node.  This allows direct connection to the
-		mesh from personal devices such as: smart phones, tablets, laptops, etc. using the chip as a 
-		router.
+		OLSR page help
 	</p>
   </div>
 </div> 
@@ -260,27 +212,13 @@
 <div data-role="page" id="page_clients" data-theme="<?php echo $theme; ?>">
   <div data-role="header">
     <a href="#page_status" class="ui-btn ui-corner-all ui-shadow ui-icon-home ui-btn-icon-left">Home</a>
-    <a href="#page_olsr_info" data-rel="popup" class="ui-btn ui-corner-all ui-shadow ui-icon-info ui-btn-icon-left">Help</a>
+    <a href="#page_clients_info" data-rel="popup" class="ui-btn ui-corner-all ui-shadow ui-icon-info ui-btn-icon-left">Help</a>
     <h1><?php echo shell_exec("hostname"); ?> - Micro Mesh</h1>
     <div data-role="navbar">
       <ul>
         <li><a href="#page_status" data-transition="reverse">Status</a></li>
-        <?php
-            switch (1) {
-                case (file_exists('/var/www/flags/.micromesh')) :
-                    ?>
-                    <li><a href="#page_nodes">Nodes</a></li>
-                    <li><a href="#page_olsr" data-transition="slide">OLSR</a></li>
-                    <?php
-                    break;
-                case (file_exists('/var/www/flags/.microrouter')) :
-                    ?>
-                    <li><a href="#page_clients" class="ui-btn-active">Clients</a></li>
-                    <?php
-                    break;
-            }
-        ?>
-        <li><a href="/setup.php" data-ajax="false" data-transition="slide">Setup</a></li>
+        <li><a href="#page_clients" class="ui-btn-active">Clients</a></li>
+        <li><a href="#page_admin" data-transition="slide">Admin</a></li>
       </ul>
     </div>
   </div>
@@ -293,29 +231,124 @@
   <div data-role="footer">
     <h1>Valley Digital Network (VDN)</h1>
   </div>
-  <div data-role="popup" id="page_olsr_info">
+  <div data-role="popup" id="page_clients_info">
     <a href="#" data-rel="back" class="ui-btn ui-corner-all ui-shadow ui-btn ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a>
     <br />
-    <p> There are two types of installations that can be selected. </p>
     <p>
-        The <b>Micro Mesh Node</b> installation will create a mesh node using one of the two wifi radios
-        available on the chip. This node will be able to communicate with other mesh nodes in range over
-        that wifi radio.  The remaining wifi radio on the chip will be used as an Access Point, which
-        allows direct connection to the mesh from personal devices such as: smart phones, tablets, laptops,
-        etc.  If an ethernet connection is also available on the chip, it can be used to provide WAN or LAN
-        access.
-    </p>
-    <p>
-        The <b>Micro Mesh Router</b> installation does not create a mesh node.  Instead, this choice will
-        only create an Access Point on the chip using one of the wifi radios.  Additionally, the chip will
-        need to have an ethernet connection to a LAN port on an existing mesh node.  All traffic from the
-        Access Point is routed to the ethernet connected mesh node.  This allows direct connection to the
-        mesh from personal devices such as: smart phones, tablets, laptops, etc. using the chip as a
-        router.
+		Clients page help
     </p>
   </div>
 </div>
 
+<div data-role="page" id="page_admin" data-theme="<?php echo $theme; ?>">
+  <div data-role="header">
+    <a href="#page_status" class="ui-btn ui-corner-all ui-shadow ui-icon-home ui-btn-icon-left">Home</a>
+    <a href="#page_admin_info" data-rel="popup" class="ui-btn ui-corner-all ui-shadow ui-icon-info ui-btn-icon-left">Help</a>
+    <h1><?php echo shell_exec("hostname"); ?> - Micro Mesh</h1>
+    <div data-role="navbar">
+      <ul>
+        <li><a href="#page_status" data-transition="reverse">Status</a></li>
+        <?php
+            switch (1) {
+                case (file_exists('/var/www/flags/.micromesh')) :
+                    ?>  
+                    <li><a href="#page_nodes" data-transition="reverse">Nodes</a></li>
+                    <li><a href="#page_olsr" data-transition="reverse">OLSR</a></li>
+                    <?php
+                    break;
+                case (file_exists('/var/www/flags/.microrouter')) :
+                    ?>  
+                    <li><a href="#page_clients" data-transition="reverse">Clients</a></li>
+                    <?php
+                    break;
+            }   
+        ?>  
+        <li><a href="#page_admin" class="ui-btn-active">Admin</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div data-role="main" class="ui-content">
+	<h2>Admin Options</h2>
+    <?php
+        switch (1) {
+            case (file_exists('/var/www/flags/.micromesh')) :
+                ?>
+                <!-- -->
+                <?php
+                break;
+            case (file_exists('/var/www/flags/.microrouter')) :
+                ?>
+                <!-- -->
+		        <div class="ui-field-contain">
+    				<label id="label_routerhostname" for="routerhostname"> Router Name:</label>
+    				<input type="text" name="routerhostname" id="routerhostname" value="<?php echo shell_exec("hostname"); ?>">
+    				<span> <i>Please make sure it is unique (e.g. microrouter01, microrouter02, etc...)</i> </span>
+				</div>
+                <?php
+                break;
+        }
+    ?>
+	<div class="ui-field-contain">
+    	<label for="adminpassword">Admin Password:</label>
+    	<input type="text" name="adminpassword" id="adminpassword" placeholder="Enter new admin password (optional)">
+	</div>
+
+    <?php
+        switch (1) {
+            case (file_exists('/var/www/flags/.micromesh')) :
+                ?>
+				<!-- -->
+                <?php
+                break;
+            case (file_exists('/var/www/flags/.microrouter')) :
+                ?>
+				<!-- -->
+		        <h2> Access Point </h2>
+		    	<fieldset data-role="controlgroup">
+		    		<legend>Choose your router mode:</legend>
+		        	<label for="hotspot">Acts as WiFi Hotspot</label>
+		        	<input type="radio" name="router_mode" id="hotspot" value="hotspot">
+		        	<label for="bridge">Connects to a WiFi Hotspot</label>
+		        	<input type="radio" name="router_mode" id="bridge" value="bridge">
+		    	</fieldset>
+		        <div class="ui-field-contain">
+		            <label for="accesspointssid">SSID:</label>
+		            <input type="text" name="accesspointssid" id="accesspointssid" value="<?php echo shell_exec("cat /etc/hostapd.conf|grep ssid|cut -d'=' -f2"); ?>" placeholder="SSID used by the access point">
+		            <label for="accesspointpassword">Password:</label>
+		            <input type="text" name="accesspointpassword" id="accesspointpassword" value="<?php echo shell_exec("cat /etc/hostapd.conf|grep wpa_passphrase|cut -d'=' -f2"); ?>" placeholder="This is the password used to access the access point">
+		            <label for="accesspointchannel">Select Channel</label>
+    		        <select name="accesspointchannel" id="accesspointchannel">
+    		            <?php echo shell_exec("/var/www/html/./wifiscan CH 2>&1"); ?>
+            		</select>
+		        </div>
+
+                <?php
+                break;
+        }
+    ?>
+
+
+
+    <a href="#resetPopup" data-rel="popup" data-transition="slideup">Reset this device!</a>
+    <div data-role="popup" id="resetPopup" class="ui-content">
+        <p>Are you sure you wish to reset this device?</p>
+        <li><a href="/setup.php" data-ajax="false" data-transition="slide">Reset this device!</a></li>
+    </div>
+
+
+  </div>
+  <div data-role="footer">
+    <h1>Valley Digital Network (VDN)</h1>
+  </div>
+  <div data-role="popup" id="page_admin_info">
+    <a href="#" data-rel="back" class="ui-btn ui-corner-all ui-shadow ui-btn ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a>
+    <br />
+    <p> 
+		Admin page help
+    </p>
+  </div>
+</div>
 
 
 </body>
