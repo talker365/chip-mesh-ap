cd87ceab00b8819f69b99dc36a0aa00a32d11bc2
Ristola <n4ldr@arrl.net>
2017-06-19 01:05:15 -0400

    Another test

--- projects/install-ads-b.sh
+++ projects/install-ads-b.sh
@@ -1,7 +1,8 @@
-
+# Project Title: ADS-B (FlightRadar 24)
+# Project Desc: ADS-B Project. This line is possibly for the Projects Tab, to grab a description directly from the installation script.
 #!/bin/bash
-#PROJECT ADS-B 
 
+clear
 # Stop on first error
 set -e
 
@@ -9,10 +10,15 @@ if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
+echo "|||||||||||||||||||||||||||||||||||||||||"
+echo "|||||||                          ||||||||"
+echo "||||||| INSTALLING ADS-B PROJECT ||||||||"
+echo "|||||||                          ||||||||"
+echo "|||||||||||||||||||||||||||||||||||||||||\n"
+echo -e "Performing an APT update.... "
 
-echo "Performing an APT update.... "
 # Update APT cache and install feeder software
 apt-get update -y
 
 echo "Installation and configuration completed!"
-echo -e "/nTHIS IS ONLY A TEST"
+echo -e "\nTHIS WAS ONLY A TEST\n"
